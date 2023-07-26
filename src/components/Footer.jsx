53e2701942f0f14b98b93e695c25100ae2d020d7
vaddisanjeevkumar9676@gmail.com <vaddisanjeevkumar9676@gmail.com>
2023-07-26 21:30:00 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Layout } from 'antd';
+import React from "react";
+import { Layout } from "antd";
 
 const { Footer } = Layout;
 
 const FooterSection = () => {
   return (
-    <Footer style={{ color: 'black', height: '100px' }}>
-      Plain text footer
+    <Footer style={{ color: "black", height: "100px" }}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+      mollitia sint. Quo necessitatibus veniam excepturi velit, consectetur
+      mollitia similique, amet ut nostrum nulla distinctio veritatis tempora
+      voluptatum aspernatur obcaecati ea.
     </Footer>
   );
 };
